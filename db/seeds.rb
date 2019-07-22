1346fd483414ebdb58fef5cfa253b3e958b6ce53
grimyoung <samhyukim@gmail.com>
2019-07-21 18:51:40 -0700

    fixed database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,6 @@ end
 
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::Lorem.sentences(5)
+  content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
